02bd6fbf17318819ea6333a309e99db917113867
Michel <michelrummens@hotmail.com>
2020-03-26 11:40:46 +0000

    Added gbc traversal

--- compiler/src/codegen/gen_byte_code.c
+++ compiler/src/codegen/gen_byte_code.c
@@ -4,6 +4,277 @@
 #include "tree_basic.h"
 #include "traverse.h"
 #include "dbug.h"
+#include "ctinfo.h"
+
+
+node *GBCprogram(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCprogram");
+    DBUG_PRINT("GBC", ("GBCprogram"));
+
+    TRAVdo(PROGRAM_DECLS(arg_node), arg_info);
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCsymboltable(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCsymboltable");
+    DBUG_PRINT("GBC", ("GBCsymboltable"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCsymboltableentry(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCsymboltableentry");
+    DBUG_PRINT("GBC", ("GBCsymboltableentry"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCdecls(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCdecls");
+    DBUG_PRINT("GBC", ("GBCdecls"));
+
+    TRAVopt(DECLS_NEXT(arg_node), arg_info);
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCexprs(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCexprs");
+    DBUG_PRINT("GBC", ("GBCexprs"));
+
+    TRAVopt(EXPRS_NEXT(arg_node), arg_info);
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCarrexpr(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCarrexpr");
+    DBUG_PRINT("GBC", ("GBCarrexpr"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCids(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCids");
+    DBUG_PRINT("GBC", ("GBCids"));
+
+    TRAVopt(IDS_NEXT(arg_node), arg_info);
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCexprstmt(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCexprstmt");
+    DBUG_PRINT("GBC", ("GBCexprstmt"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCreturn(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCreturn");
+    DBUG_PRINT("GBC", ("GBCreturn"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCfuncall(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCfuncall");
+    DBUG_PRINT("GBC", ("GBCfuncall"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCcast(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCcast");
+    DBUG_PRINT("GBC", ("GBCcast"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCfundefs(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCfundefs");
+    DBUG_PRINT("GBC", ("GBCfundefs"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCfundef(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCfundef");
+    DBUG_PRINT("GBC", ("GBCfundef"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCfunbody(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCfunbody");
+    DBUG_PRINT("GBC", ("GBCfunbody"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCifelse(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCifelse");
+    DBUG_PRINT("GBC", ("GBCifelse"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCwhile(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCwhile");
+    DBUG_PRINT("GBC", ("GBCwhile"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCdowhile(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCdowhile");
+    DBUG_PRINT("GBC", ("GBCdowhile"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCfor(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCfor");
+    DBUG_PRINT("GBC", ("GBCfor"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCglobdecl(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCglobdecl");
+    DBUG_PRINT("GBC", ("GBCglobdecl"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCglobdef(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCglobdef");
+    DBUG_PRINT("GBC", ("GBCglobdef"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCparam(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCparam");
+    DBUG_PRINT("GBC", ("GBCparam"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCvardecl(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCvardecl");
+    DBUG_PRINT("GBC", ("GBCvardecl"));
+
+    CTInote("MOOI HEAAA");
+    printf("%s", VARDECL_NAME(arg_node)); 
+
+    TRAVopt(VARDECL_NEXT(arg_node), arg_info);
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCstmts(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCstmts");
+    DBUG_PRINT("GBC", ("GBCstmts"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCassign(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCassign");
+    DBUG_PRINT("GBC", ("GBCassign"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCbinop(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCbinop");
+    DBUG_PRINT("GBC", ("GBCbinop"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCmonop(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCmonop");
+    DBUG_PRINT("GBC", ("GBCmonop"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCvarlet(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCvarlet");
+    DBUG_PRINT("GBC", ("GBCvarlet"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCvar(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCvar");
+    DBUG_PRINT("GBC", ("GBCvar"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCnum(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCnum");
+    DBUG_PRINT("GBC", ("GBCnum"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCfloat(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCfloat");
+    DBUG_PRINT("GBC", ("GBCfloat"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCbool(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCbool");
+    DBUG_PRINT("GBC", ("GBCbool"));
+
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCerror(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCerror");
+    DBUG_PRINT("GBC", ("GBCerror"));
+
+    DBUG_RETURN(arg_node);
+}
 
 
 /*
@@ -13,10 +284,19 @@
 node *GBCdoGenByteCode( node *syntaxtree)
 {
   DBUG_ENTER("GBCdoGenByteCode");
+  DBUG_PRINT("GBC", ("GBCdoGenByteCode"));
 
-  /*
-   * Your work here....
-   */
+  // info *info = MakeInfo();
+
+  printf("%s", "HELLO_FRESH");
+  CTInote("Sum of additions: %d", 100);
+
+  TRAVpush(TR_gbc);
+  syntaxtree = TRAVdo(syntaxtree, NULL);
+  TRAVpop();
+
+  // free the pointer
+  // FreeInfo(info);
 
   DBUG_RETURN( syntaxtree);
 }
--- compiler/src/codegen/gen_byte_code.h
+++ compiler/src/codegen/gen_byte_code.h
@@ -2,6 +2,39 @@
 #define _GEN_BYTE_CODE_H_
 #include "types.h"
 
+extern node *GBCprogram (node *arg_node, info *arg_info);
+extern node *GBCsymboltable (node *arg_node, info *arg_info);
+extern node *GBCsymboltableentry (node *arg_node, info *arg_info);
+extern node *GBCdecls (node *arg_node, info *arg_info);
+extern node *GBCexprs (node *arg_node, info *arg_info);
+extern node *GBCarrexpr (node *arg_node, info *arg_info);
+extern node *GBCids (node *arg_node, info *arg_info);
+extern node *GBCexprstmt (node *arg_node, info *arg_info);
+extern node *GBCreturn (node *arg_node, info *arg_info);
+extern node *GBCfuncall (node *arg_node, info *arg_info);
+extern node *GBCcast (node *arg_node, info *arg_info);
+extern node *GBCfundefs (node *arg_node, info *arg_info);
+extern node *GBCfundef (node *arg_node, info *arg_info);
+extern node *GBCfunbody (node *arg_node, info *arg_info);
+extern node *GBCifelse (node *arg_node, info *arg_info);
+extern node *GBCwhile (node *arg_node, info *arg_info);
+extern node *GBCdowhile (node *arg_node, info *arg_info);
+extern node *GBCfor (node *arg_node, info *arg_info);
+extern node *GBCglobdecl (node *arg_node, info *arg_info);
+extern node *GBCglobdef (node *arg_node, info *arg_info);
+extern node *GBCparam (node *arg_node, info *arg_info);
+extern node *GBCvardecl (node *arg_node, info *arg_info);
+extern node *GBCstmts (node *arg_node, info *arg_info);
+extern node *GBCassign (node *arg_node, info *arg_info);
+extern node *GBCbinop (node *arg_node, info *arg_info);
+extern node *GBCmonop (node *arg_node, info *arg_info);
+extern node *GBCvarlet (node *arg_node, info *arg_info);
+extern node *GBCvar (node *arg_node, info *arg_info);
+extern node *GBCnum (node *arg_node, info *arg_info);
+extern node *GBCfloat (node *arg_node, info *arg_info);
+extern node *GBCbool (node *arg_node, info *arg_info);
+extern node *GBCerror (node *arg_node, info *arg_info);
+
 extern node *GBCdoGenByteCode( node *syntaxtree);
 
 #endif
